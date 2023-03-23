54edb1efee18bf73b986545b9a919b7bce0c2aa7
Gabriel Souza de Carvalho Lima <gabrielsclima.2004@gmail.com>
2023-03-23 12:58:13 -0300

    Files ready

---
+++ script.js
@@ -0,0 +1,11 @@
+function verde(){
+    document.getElementById("fundo").style.backgroundColor = "green";
+    document.getElementById("fundo").style.color = "white";
+    document.getElementById("imagemFundo").src = "images/Proa_Tech_Fundo_Zoom copia.png";
+}
+
+function azul(){
+    document.getElementById("fundo").style.backgroundColor = "blue";
+    document.getElementById("fundo").style.color = "yellow";
+    document.getElementById("imagemFundo").src = "images/SpeedTech.png";
+}
